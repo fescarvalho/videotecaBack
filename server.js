40b4a2fa65d6a59303f98f0a455b41abe92ed680
Fernando Carvalho <fernando_scarvalho@hotmail.com>
2022-08-01 22:51:21 -0300

    server.js altered

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-const routes = require("./routes");
-const connectToDatabase = require("./database");
+const routes = require("./src/routes");
+const connectToDatabase = require("./src/database");
 
 connectToDatabase();
 
